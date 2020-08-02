d276a58450fb5f2f0eca25b31db55f6eca7c0621
toskaushik <kaushik.sharad@gmail.com>
2020-08-02 13:30:59 -0400

    Add files via upload

--- hospitalization.js
+++ hospitalization.js
@@ -138,7 +138,7 @@ async function pageOnload() {
     .on("mouseover", function (d) {
       div.transition().duration(200).style("opacity", 0.9);
       div
-        .html("Week 2020/" + d.week + "<br/>" + "Percentage " + d.count + "%")
+        .html("Week 2020/" + d.week + "<br/>" + "Percentage " + d.value + "%")
         .style("left", d3.event.pageX + "px")
         .style("top", d3.event.pageY - 28 + "px");
     })
